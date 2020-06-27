fa8ad746a3a69513deeefdcc64f7926579b370b2
Lena <lzhengem@gmail.com>
2020-06-26 23:14:35 -0700

    added storybook

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { Button } from '@storybook/react/demo';
 
-export default { title: 'Button' };
+export default { title: 'Test' };
 
-export const withText = () => <Button>Hello Button</Button>;
-
-export const withEmoji = () => (
-  <Button>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
+export const SampleText = () => (
+    <button>I like turtles</button>
 );
