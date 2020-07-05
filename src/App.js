add2b8a671d728fd84ce5e55e6029adb46104e8b
Lena Zheng <lzhengem@gmail.com>
2020-07-04 22:11:41 -0700

    added amadeus (#12)
    * added amadeus
    
    * use getAmadeus
    
    * removed axios call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState, useEffect } from 'react'
+import React from 'react'
+// import { getAmadeus } from './lib/amadeus'
+import './App.css'
 
-function App() {
+function App () {
+  // const [test, setTest] = useState([])
+  // useEffect(() => {
+  //   getAmadeus().then(answer => {
+  //     setTest(answer.data)
+  //   })
+  // }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      {/* {test.map((item) =>{
+      return item.id
+    })} */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/lib/amadeus.jsx
@@ -0,0 +1,16 @@
+import Amadeus from 'amadeus'
+
+export async function getAmadeus () {
+  const amadeus = new Amadeus({
+    clientId: process.env.REACT_APP_AMADEUS_API_KEY,
+    clientSecret: process.env.REACT_APP_AMADEUS_API_SECRET
+  })
+  
+  const test = await amadeus.shopping.flightOffersSearch.get({
+    originLocationCode: 'SYD',
+    destinationLocationCode: 'BKK',
+    departureDate: '2020-08-01',
+    adults: '2'
+  })
+  return test;
+}
