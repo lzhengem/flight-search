72da9c40bcf263de31e20a9f2d2f6046cfd13a7b
Lena <lzhengem@gmail.com>
2020-07-04 20:22:54 -0700

    use getAmadeus

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getAmadeus } from './lib/amadeus'
 import './App.css'
 
 function App () {
+
+  const [test,setTest] = useState([]);
+  useEffect(() => { 
+    getAmadeus().then(answer => {
+      setTest(answer.data)
+    })
+  },[])
+
   return (
     <div className="App">
-    Hello!
+    {test.map((item) =>{
+      return item.id
+    })}
     </div>
   )
 }
--- src/lib/amadeus.jsx
+++ src/lib/amadeus.jsx
@@ -1,19 +1,16 @@
 import Amadeus from 'amadeus'
 
-export function getAmadeus () {
+export async function getAmadeus () {
   const amadeus = new Amadeus({
     clientId: process.env.REACT_APP_AMADEUS_API_KEY,
     clientSecret: process.env.REACT_APP_AMADEUS_API_SECRET
   })
-
-  amadeus.shopping.flightOffersSearch.get({
+  
+  const test = await amadeus.shopping.flightOffersSearch.get({
     originLocationCode: 'SYD',
     destinationLocationCode: 'BKK',
     departureDate: '2020-08-01',
     adults: '2'
-  }).then(function (response) {
-    console.log(response.data)
-  }).catch(function (responseError) {
-    console.log(responseError.code)
   })
+  return test;
 }
