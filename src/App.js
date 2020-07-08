1feef4db9ed759dde013f5ce3dee54aa91652d1d
Lena <lzhengem@gmail.com>
2020-07-07 19:18:02 -0700

    add ui to select endpoints

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { connect } from 'react-redux'
 import { PropTypes } from 'prop-types'
+import { flightOptions } from './flightOptions'
 
 function App ({ dispatch, flights }) {
+  const [payload, setPayload] = useState({})
+
+  const handleOnChange = (e) => {
+    const newPayload = { ...payload, [e.target.name]: e.target.value }
+    if (e.target.name === 'origin') {
+      delete newPayload.destination
+    }
+    console.log({ newPayload })
+    setPayload(newPayload)
+  }
+
   return (
     <div className="App">
-      <div onClick={() => { dispatch({ type: 'FLIGHT_SEARCH_REQUESTED' }) }}>Request some flights</div>
+      Origin:
+      <select name="origin" onChange={handleOnChange}>
+        {[''].concat(Object.keys(flightOptions)).map((origin) => {
+          return <option key={origin} value={origin}>{origin}</option>
+        })}
+      </select>
+      Destination:
+      <select name="destination" onChange={handleOnChange}>
+        {payload.origin &&
+        [''].concat(flightOptions[payload.origin]).map((destination) => {
+          return <option key={destination} value={destination}>{destination}</option>
+        })}
+      </select>
+
+      Date: <input name="date" placeholder="2020-08-01" onChange={handleOnChange}/>
+      Adults: <input name="adults" onChange={handleOnChange}/>
+
+      <div onClick={() => { dispatch({ type: 'FLIGHT_SEARCH_REQUESTED', payload: payload }) }}>Request some flights</div>
       {flights.map((flight) => {
         return (JSON.stringify(flight))
       })}
