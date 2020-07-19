bf3aa2b30982201d4705a84dcf97416bc2f3e7d1
Lena <lzhengem@gmail.com>
2020-07-19 14:13:30 -0700

    tailwind css to storybook

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// if any configuration of tailwind is needed
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ["Gilroy", "sans-serif"],
+      body: ["Graphik", "sans-serif"]
+    },
+    extend: {
+      colors: {
+        myfave: "#9cdbff"
+      }
+    }
+  }
+}
