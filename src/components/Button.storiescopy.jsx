ce6a5b0cc250fdf2c76484b3cf00ba5194ffa286
Lena Zheng <lzhengem@gmail.com>
2020-07-19 15:06:28 -0700

    Features/drop down component 173764133 (#20)
    * WIP
    
    * add drop down and updated storybook
    
    * added action to storybook

--- src/components/Button.storiescopy.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default { title: 'Test' }
-
-export const SampleText = () => <button>I like turtles</button>
---
+++ src/components/dropDown.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+const DropDown = ({ width = "base", options, name, onChange }) => {
+  let widthCss
+  switch (width) {
+    case "largest": {
+      widthCss = "w-64"
+      break
+    }
+    case "large": {
+      widthCss = "w-48"
+      break
+    }
+    case "base": {
+      widthCss = "w-32"
+      break
+    }
+    default:
+      widthCss = "w-32"
+  }
+  return (
+    <select
+      className={`${widthCss} border border-gray-500 rounded`}
+      onChange={onChange}
+    >
+      {options.map((option) => {
+        return (
+          <option name={name} value={option}>
+            {option}
+          </option>
+        )
+      })}
+    </select>
+  )
+}
+
+export { DropDown }
